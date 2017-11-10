ec6cb8da5dacf8e9efc4e73c330c98e5c943c847
BizimoskiDa <daniel.bizimoski@rpn.ch>
2017-11-10 11:10:10 +0100

    Create the main source, for my skeleton

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +10,59 @@ namespace Calculatrice
     {
         static void Main(string[] args)
         {
+            double dblVal1, dblVal2, dblRep = 0;
+            bool isOpIncorrect = true;
+
+            Console.WriteLine("Calculatrice;");
+            Console.WriteLine("-------------");
+
+            //Saisie des opérandes, avec vérification de ces dernières
+            Console.Write("Saisir la première valeur :");
+            while(!double.TryParse(Console.ReadLine(), out dblVal1))
+            {
+                Console.Write("Saisir à nouveau la 1ère valeur :");
+            }
+            
+            Console.Write("Saisir la deuxième valeur :");
+            while (!double.TryParse(Console.ReadLine(), out dblVal2))
+            {
+                Console.Write("Saisir à nouveau la 2eme valeur :");
+            }
+
+            //Saisie du choix de l'opérateur
+            Console.Write("Opération à choix [+ - * / '] :");
+            while (isOpIncorrect)
+            {
+                string strOp = Console.ReadLine();
+                isOpIncorrect = false;
+                switch (strOp)
+                {
+                    default:
+                        Console.WriteLine("Opération non reconnue par le système");
+                        Console.Write("Veuillez saisir à nouveau l'opérateur [+ - * / '] :");
+                        isOpIncorrect = true;
+                        break;
+                    case "+":
+                        dblRep = 0;
+                        break;
+                    case "-":
+                        dblRep = 0;
+                        break;
+                    case "*":
+                        dblRep = 0;
+                        break;
+                    case "/":
+                        dblRep = 0;
+                        break;
+                    case "'":
+                        dblRep = 0;
+                        break;
+                }
+            }
+
+            Console.Write(dblVal1.ToString());
+
+            Console.ReadKey();
         }
     }
 }
